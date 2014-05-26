9480cf7f64d8f660fbbfaa446487fbf0e506e448
neutmute <neutmute@users.noreply.github.com>
2014-05-26 21:45:44 +1000

    first commit

---
+++ ClockWatcher/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Security;
+using System.Text;
+using System.Collections.Generic;
+using Kraken.Core;
+
+namespace ClockWatcher
+{
+    class Program
+    {
+        private static ObjectDump GetDump(EventLogEntry entry)
+        {
+            var dump = new ObjectDump();
+            dump.Headers.Add("Time");
+            dump.Headers.Add("Message");
+            dump.Data.Add(entry.TimeGenerated.ToString("dd-MMM-yyyy HH:mm"));
+            dump.Data.Add(entry.Message);
+            return dump;
+        }
+
+        static void Main(string[] args)
+        {
+
+            string logType = "security";
+            var lockUnlockEVentTypes = new long[] { 4800, 4801 };
+            var machine = "."; // local machine
+            
+            var securityLog = new EventLog(logType, machine);
+
+            var logEntries = (
+                            from EventLogEntry logEntry in securityLog.Entries
+                            where lockUnlockEVentTypes.Contains(logEntry.InstanceId)
+                            select logEntry
+                        )
+                        .OrderByDescending(e => e.TimeGenerated)
+                        .ToList();
+
+            var dumper = new ObjectDumper<EventLogEntry>(GetDump);
+            Console.WriteLine(dumper.Dump(logEntries));
+        }
+    }
+}
+
